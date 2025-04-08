76ed02f71c866fc78e30fefff1b175f5ee4403ce
Dylan-Sams <samsds@mail.uc.edu>
2025-04-08 13:11:20 -0400

    Add project files.

---
+++ Malibu_Assignment10/APIPackage/API.py
@@ -0,0 +1,13 @@
+# API.py
+# Student Name: Dylan Sams, Alisha Siddiqui, Colton Ramsey, Leah Radcliffe
+# email:  samsds@mail.uc.edu, ramseyc6@mail.uc.edu, radclilr@mail.uc.edu, siddiqas@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date:   4/10/2025
+# Course #/Section:  IS4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment: In this assignment we will execute an API to get results from, then we will extract some interesting data to print to the console and write to a CSV file.
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant
